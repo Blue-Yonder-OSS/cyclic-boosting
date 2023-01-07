a9ad9b41b12dc3f115fde22cde45d62f1fa987f2
Felix Wick <felix.wick@gmx.net>
2023-01-07 13:12:03 +0100

    missing description for pypi

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ from setuptools import setup, find_packages
 
 import os
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
 def get_install_requirements(path):
     content = open(os.path.join(os.path.dirname(__file__), path)).read()
     return [req for req in content.split("\n") if req != "" and not req.startswith("#")]
@@ -14,7 +18,9 @@ def setup_package():
         install_requires=get_install_requirements("requirements.txt"),
         packages=find_packages(),
         classifiers=["Programming Language :: Python"],
-        use_scm_version=True
+        use_scm_version=True,
+        long_description=long_description,
+        long_description_content_type='text/markdown'
     )
 
 if __name__ == "__main__":
