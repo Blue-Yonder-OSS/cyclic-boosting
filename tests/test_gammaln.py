589c7e20a53d134d82ec7ce20d887fc2e875c347
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2023-04-22 16:48:59 +0200

    clean up test

--- tests/test_gammaln.py
+++ tests/test_gammaln.py
@@ -3,11 +3,6 @@ from math import log, exp
 
 
 def test_gammaln():
-    print(exp(gammaln(5.0)))
-    print(gammaln(4.0))
-    print(gammaln(3.0))
-    print(gammaln(2.0))
-    print(gammaln(1.0))
     assert abs(gammaln(5.0) - log(24.0)) < 1e-6
     assert abs(gammaln(4.0) - log(6.0)) < 1e-6
     assert abs(gammaln(1.0) - log(1.0)) < 1e-6
