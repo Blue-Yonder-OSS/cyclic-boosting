c37a07863d0545b94f3ed792cb7d6cd1fa95f1ad
Stefan Ulbrich <6009224+StefanUlbrich@users.noreply.github.com>
2023-04-23 23:53:54 +0200

    Update tests

--- tests/test_gammaln.py
+++ tests/test_gammaln.py
@@ -1,9 +1,16 @@
-from cyclic_boosting.nbinom import gammaln
+from cyclic_boosting.nbinom import gammaln, GAMMALN_CONSTANT
 from math import log, exp
+from scipy import special
+import numpy as np
 
 
 def test_gammaln():
-    assert abs(gammaln(5.0) - log(24.0)) < 1e-6
-    assert abs(gammaln(4.0) - log(6.0)) < 1e-6
-    assert abs(gammaln(1.0) - log(1.0)) < 1e-6
-    assert abs(gammaln(2.0) - log(1.0)) < 1e-6
+    data = np.linspace(1e-04, 2 - 1e-02, 100)
+    tol = (GAMMALN_CONSTANT + 1) * np.exp(-GAMMALN_CONSTANT)
+    for x in data:
+        assert abs(gammaln(x) - special.gammaln(x)) < tol, f"for x={x}"
+
+    data = np.linspace(10,1000, 10000)
+    tol = 1e-01
+    for x in data:
+        assert abs(gammaln(x) - special.gammaln(x)) < tol, f"for x={x}"
