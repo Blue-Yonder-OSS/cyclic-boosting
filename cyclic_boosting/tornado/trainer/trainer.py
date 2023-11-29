93ca1bfd6c2e9e653228cef2d87966cfbd8d2e7f
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2023-11-29 14:31:04 +0900

    [mod] avoid duplication and refactoring

--- cyclic_boosting/tornado/trainer/trainer.py
+++ cyclic_boosting/tornado/trainer/trainer.py
@@ -1,12 +1,12 @@
 import logging
 
-import six
 import abc
 import copy
+import six
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-from .evaluator import EvaluatorBase
+from .evaluator import Evaluator
 from .logger import Logger
 
 
@@ -14,146 +14,97 @@ _logger = logging.getLogger(__name__)
 _logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter(fmt="%(message)s"))
-handler.terminator = ''
+handler.terminator = ""
 _logger.addHandler(handler)
 
 
 @six.add_metaclass(abc.ABCMeta)
-class TrainerBase():
+class TrainerBase:
     def __init__(self, DataModule, TornadoModule):
         self.data_deliveler = DataModule
         self.manager = TornadoModule
 
     @abc.abstractmethod
-    def run(self,
-            target,
-            test_size=0.2,
-            seed=0,
-            save_dir='./models',
-            log_policy='vote',
-            metrix=None,
-            verbose=True
-            ):
+    def run(self, target, test_size=0.2, seed=0, save_dir="./models", log_policy="vote", verbose=True):
         pass
+
+    def train(self, target, valid_data, logger, evaluator, verbose):
+        while self.manager.manage():
+            estimater = self.manager.build()
+            # train
+            X = copy.deepcopy(self.manager.X)
+            y = copy.deepcopy(self.manager.y)
+            _ = estimater.fit(X, y)
+
+            # validation
+            y_valid = np.asarray(valid_data[target])
+            X_valid = valid_data.drop(target, axis=1)
+            yhat = estimater.predict(X_valid)
+            evaluator.eval_all(y_valid, yhat, estimater, verbose)
+
+            # log
+            logger.log(estimater, evaluator, self.manager)
+            # self.manager.reset(train, target)
 
 
 class Trainer(TrainerBase):
     def __init__(self, DataModule, TornadoModule):
         super().__init__(DataModule, TornadoModule)
 
-    def run(self,
-            target,
-            test_size=0.2,
-            seed=0,
-            save_dir='./models',
-            log_policy='vote_by_num',
-            metrix=None,
-            verbose=True):
-        dataset = self.data_deliveler.generate()
-        train, validation = train_test_split(
-            dataset,
-            test_size=test_size,
-            random_state=seed)
-
-        evaluator = EvaluatorBase()
+    def run(self, target, test_size=0.2, seed=0, save_dir="./models", log_policy="COD", verbose=True):
+        # initialize
+        evaluator = Evaluator()
         logger = Logger(save_dir, log_policy)
-
+        dataset = self.data_deliveler.generate()
+        train, validation = train_test_split(dataset, test_size=test_size, random_state=seed)
         self.manager.init(train, target)
-        while self.manager.manage():
-            estimater = self.manager.build()
-            # train
-            X = copy.deepcopy(self.manager.X)
-            y = copy.deepcopy(self.manager.y)
-            _ = estimater.fit(X, y)
 
-            # validation
-            y_valid = np.asarray(validation[target])
-            X_valid = validation.drop(target, axis=1)
-            yhat = estimater.predict(X_valid)
-            evaluator.eval_all(y_valid, yhat, estimater, verbose)
-
-            # log
-            logger.log(estimater, evaluator, self.manager)
+        # train
+        self.train(target, validation, logger, evaluator, verbose)
 
 
 class SqueezeTrainer(TrainerBase):
     def __init__(self, DataModule, TornadoModule):
         super().__init__(DataModule, TornadoModule)
+        self.policy = ["COD", "vote_by_num", "vote"]
 
-    def run(self,
-            target,
-            test_size=0.2,
-            seed=0,
-            save_dir='./models',
-            log_policy='compute_COD',
-            metrix=None,
-            verbose=True
-            ):
-        dataset = self.data_deliveler.generate()
-        train, validation = train_test_split(
-            dataset,
-            test_size=test_size,
-            random_state=seed)
-
-        evaluator = EvaluatorBase()
+    def run(self, target, test_size=0.2, seed=0, save_dir="./models", log_policy="COD", verbose=True):
+        # initialize
+        evaluator = Evaluator()
         logger = Logger(save_dir, log_policy)
-
+        dataset = self.data_deliveler.generate()
+        train, validation = train_test_split(dataset, test_size=test_size, random_state=seed)
         self.manager.init(train, target)
-        while self.manager.manage():
-            estimater = self.manager.build()
-            # train
-            X = copy.deepcopy(self.manager.X)
-            y = copy.deepcopy(self.manager.y)
-            _ = estimater.fit(X, y)
 
-            # validation
-            y_valid = np.asarray(validation[target])
-            X_valid = validation.drop(target, axis=1)
-            yhat = estimater.predict(X_valid)
-            evaluator.eval_all(y_valid, yhat, estimater, verbose)
+        # sigle variable regression analysis for variable selection
+        self.train(target, validation, logger, evaluator, verbose)
 
-            # log
-            logger.log(estimater, evaluator, self.manager)
+        truncated_features = {}
+        # ここのthresholdは自由に決めることができるが基本的に２が良いらしい
+        threshold = 2
 
-            # self.manager.reset(train, target)
+        # valid interaction term selection
+        base_explanatory_variables = []
+        for feature, data in logger.sorted_CODs:
+            # original variables
+            # NOTE: is it better including original variables for truncating?
+            if isinstance(feature, str):
+                base_explanatory_variables.append(feature)
+            # trancate
+            if data["F"] > threshold:
+                truncated_features[feature] = data
 
-        if log_policy in ["compute_COD", "vote_by_num", "vote"]:
-            truncated_features = {}
-            #ここのthresholdは自由に決めることができるが基本的に２が良いらしい
-            threshold = 2
+        # set updated setting
+        interaction = [x for x in truncated_features.keys() if isinstance(x, tuple)]
+        explanatory_variables = base_explanatory_variables + interaction
+        self.manager.set_to_multiple(explanatory_variables)
 
-            base = []
-            for feature, data in logger.sorted_CODs:
-                if isinstance(feature, str):
-                    base.append(feature)
+        _logger.info("\nTRUNCATED\n")
+        _logger.info(f"{truncated_features}\n\n")
 
-                if data["F"] > threshold:
-                    truncated_features[feature] = data
+        # clearing for next training
+        logger.reset_count()
+        evaluator.clear()
 
-            _logger.info("\nTRUNCATED\n")
-            # base = [x for x in truncated_features.keys() if isinstance(x, str)]
-            interaction = [x for x in truncated_features.keys() if isinstance(x, tuple)]
-
-            # 寄与していると判定された交互作用項(2変数)の選別
-            truncated_features = base + interaction
-            self.manager.set_to_multiple(truncated_features)
-            _logger.info(f"{truncated_features}\n\n")
-
-            logger.reset_count()
-            evaluator.clear()
-            #ここで二回書いているのは後で関数にしたい
-            while self.manager.manage():
-                estimater = self.manager.build()
-                # train
-                X = copy.deepcopy(self.manager.X)
-                y = copy.deepcopy(self.manager.y)
-                _ = estimater.fit(X.copy(), y)
-                # validation
-                y_valid = np.asarray(validation[target])
-                X_valid = validation.drop(target, axis=1)
-                yhat = estimater.predict(X_valid.copy())
-                evaluator.eval_all(y_valid, yhat, estimater, verbose)
-                # log
-                logger.log(estimater, evaluator, self.manager)
-
-                # self.manager.reset(train, target)
+        # multiple variable regression (main training)
+        self.train(target, validation, logger, evaluator, verbose)
