334c8543f095c7488fbf2c5c818e5e060a826cec
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2023-12-13 16:11:34 +0900

    [mod] quantile evaluator's logic

--- cyclic_boosting/tornado/trainer/evaluator.py
+++ cyclic_boosting/tornado/trainer/evaluator.py
@@ -1,3 +1,5 @@
+import logging
+
 import abc
 import six
 import numpy as np
@@ -14,39 +16,43 @@ from .metrics import (
     mean_pinball_loss,
 )
 
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter(fmt="%(message)s"))
+handler.terminator = ""
+_logger.addHandler(handler)
+
 
 @six.add_metaclass(abc.ABCMeta)
 class EvaluatorBase:
     def __init__(self) -> None:
-        self.result = {}
-        self.eval_funcs = {}
+        self.result = dict()
+        self.eval_funcs = dict()
 
-    def eval_all(self, y, yhat, est, verbose=True, digit=5):
-        # eval
+    def eval(self, y, yhat, est, verbose=True, digit=5):
         for metrics, func in self.eval_funcs.items():
-            if metrics not in self.result.keys():
-                self.result[metrics] = []
-
-            if not metrics == "F":
+            if metrics not in ["F", "COD"]:
                 result = func(y, yhat)
             else:
                 result = func(y, yhat, k=len(est["CB"].feature_groups))
-            self.result[metrics].append(result)
+
+            self.result[metrics] = result
 
             if verbose:
-                print(f"[{metrics}]: {round(result, digit)}")
+                _logger.info(f"[{metrics}]: {round(result, digit)}\n")
 
     def mean_metrics(self, digit=5):
         for metrics, values in self.result.items():
-            print(f"[{metrics}]: {round(np.mean(values), digit)}")
+            _logger.info(f"[{metrics}]: {round(np.mean(values), digit)}\n")
 
     def clear(self):
-        self.result = {}
+        self.result = dict()
 
 
 class Evaluator(EvaluatorBase):
     def __init__(self) -> None:
-        self.result = {}
+        self.result = dict()
         self.eval_funcs = {
             "MD": mean_deviation,
             "MAD": mean_absolute_deviation,
@@ -62,5 +68,17 @@ class Evaluator(EvaluatorBase):
 
 class QuantileEvaluator(EvaluatorBase):
     def __init__(self) -> None:
-        self.result = {}
-        self.eval_funcs = {"PINBALL": mean_pinball_loss}
+        self.result = dict()
+        self.eval_funcs = {"PINBALL": mean_pinball_loss, "COD": coefficient_of_determination}
+
+    def eval(self, y, yhat, q, verbose=True, digit=5):
+        for metrics, func in self.eval_funcs.items():
+            if metrics == "PINBALL":
+                result = func(y, yhat, q)
+            else:
+                result = func(y, yhat)
+
+            self.result[metrics] = result
+
+            if verbose:
+                _logger.info(f"[{metrics}]: {round(result, digit)}\n")
