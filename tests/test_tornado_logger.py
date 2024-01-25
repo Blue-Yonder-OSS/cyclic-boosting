8db6bc677edd5cf2861a173668f8f5f303646e6f
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2024-01-25 19:22:16 +0900

    [new] unit test for tornado modules

---
+++ tests/test_tornado_logger.py
@@ -0,0 +1,274 @@
+import glob
+import os
+import shutil
+import numpy as np
+import pandas as pd
+from cyclic_boosting.tornado.trainer.logger import (
+    Logger,
+    BFForwardLogger
+)
+from cyclic_boosting.pipelines import (
+    pipeline_CBPoissonRegressor,
+)
+from cyclic_boosting import flags, observers
+import pytest
+
+
+@pytest.fixture(scope="module")
+def prepare_log_data() -> dict:
+    log_data = {
+        "iter": 100,
+        "features": ["dummy"],
+        "feature_properties": {
+            "dummy": flags.IS_CONTINUOUS
+        },
+        "smoothers": dict(),
+        "metrics": {
+            "COD": 1.0,
+            "F": 1.0,
+            "PINBALL": 1.0
+        }
+    }
+    return log_data
+
+
+@pytest.fixture(scope="module")
+def prepare_tornado_modules() -> tuple:
+    # manager = ForwardSelectionModule()
+    manager_attr = {
+        "end": 100,
+        "experiment": 1,
+        "mode": "first",
+        "features": ["dummy"],
+        "feature_properties": {
+            "dummy": flags.IS_CONTINUOUS
+            },
+        "smoothers": dict(),
+    }
+    col = "dummy"
+    estimator = pipeline_CBPoissonRegressor(
+        feature_groups=[col],
+        feature_properties={col: flags.IS_CONTINUOUS},
+        observers=[
+            observers.PlottingObserver(iteration=1),
+            observers.PlottingObserver(iteration=-1),
+        ]
+
+    )
+    X = pd.DataFrame(np.arange(10), columns=[col])
+    y = np.arange(10)
+    estimator.fit(X, y)
+    return manager_attr, estimator
+
+
+def test_logger(prepare_log_data, prepare_tornado_modules) -> None:
+    log_data = prepare_log_data
+    manager_attr, est = prepare_tornado_modules
+
+    # parameters
+    save_dir = "./save_dir"
+    f_name = "temp"
+    policy = "COD"
+    f_round, s_round = "first", "second"
+    eval = log_data["metrics"]
+    bench_mark = {k: (v - 1) for k, v in log_data["metrics"].items()}
+
+    logger = Logger(save_dir=save_dir, policy=policy)
+
+    desired = {
+        "iter": 0,
+        "save_dir": save_dir,
+        "policy": policy,
+        "first_round": f_round,
+        "second_round": s_round,
+        "log_data": dict(),
+        "CODs": dict(),
+        "model_dir": None,
+    }
+    assert logger.get_params() == desired
+
+    params = {"log_data": log_data}
+    desired.update(params)
+    logger.set_params(params)
+    assert logger.get_params()["log_data"] == log_data
+    del desired
+
+    logger.make_dir()
+    assert os.path.isdir(save_dir)
+
+    logger.make_model_dir()
+    model_dir = os.path.join(save_dir, f"model_{log_data['iter']}")
+    assert os.path.isdir(model_dir)
+
+    ext = ".pkl"
+    logger.save_model(est, f_name + ext)
+    assert os.path.exists(f_name + ext)
+    os.remove(f_name + ext)
+
+    ext = ".txt"
+    logger.save_metrics(f_name + ext)
+    assert os.path.exists(f_name + ext)
+    os.remove(f_name + ext)
+
+    logger.save_setting(f_name + ext)
+    assert os.path.exists(f_name + ext)
+    os.remove(f_name + ext)
+
+    ext = ".pdf"
+    logger.save_plot(est, f_name + ext)
+    assert os.path.exists(f_name + ext)
+    os.remove(f_name + ext)
+
+    logger.save(est)
+    assert len(glob.glob(f"{model_dir}/*")) == 4
+    shutil.rmtree(model_dir)
+
+    logger.reset_count()
+    assert logger.get_params()["iter"] == 0
+
+    try:
+        logger.output(eval, None)
+    except Exception as error:
+        assert False, error
+
+    params = {
+        "iter": 100,
+    }
+    logger.set_params(params)
+    logger.hold(est, eval, manager_attr, verbose=True, save=True)
+    assert log_data == logger.get_params()["log_data"]
+    ext = ".pkl"
+    os.remove(os.path.join(save_dir, f_name + ext))
+
+    # check for validation
+    params = {
+        "policy": policy,
+        "bench_mark": {"metrics": bench_mark}
+    }
+    logger.set_params(params)
+    assert logger.validate(eval), f"{policy} logic is wrong"
+
+    policy = "PINBALL"
+    bench_mark = {k: (v + 1) for k, v in log_data["metrics"].items()}
+    params = {
+        "policy": policy,
+        "bench_mark": {"metrics": bench_mark}
+    }
+    logger.set_params(params)
+    assert logger.validate(eval), f"{policy} logic is wrong"
+
+    # check for 2 steps round training
+    # first round
+    param = {
+        "iter": 100,
+        "first_round": f_round,
+        "bench_mark": {"metrics": bench_mark},
+        }
+    logger.set_params(param)
+    try:
+        logger.log(est, eval, manager_attr)
+    except Exception as error:
+        assert False, error
+
+    # second round
+    param = {
+        "second_round": s_round,
+        "bench_mark": {"metrics": bench_mark},
+        }
+    logger.set_params(param)
+    param = {
+        "mode": s_round,
+        }
+    manager_attr.update(param)
+    try:
+        logger.log(est, eval, manager_attr)
+    except Exception as error:
+        assert False, error
+
+
+def test_BFForwardLogger(prepare_log_data, prepare_tornado_modules) -> None:
+    log_data = prepare_log_data
+    manager_attr, est = prepare_tornado_modules
+
+    # parameters
+    save_dir = "./save_dir"
+    f_name = "temp"
+    policy = "COD"
+    f_round, s_round = "first", "second"
+    eval = log_data["metrics"]
+    bench_mark = {k: (v - 1) for k, v in log_data["metrics"].items()}
+
+    logger = BFForwardLogger(
+        save_dir=save_dir,
+        policy=policy
+    )
+
+    param = {
+        "mode": f_round,
+        "experiment": 1,
+        "end": 1,
+        "features": ["dummy"],
+        }
+    manager_attr.update(param)
+    try:
+        logger.output(eval, manager_attr)
+    except Exception as error:
+        assert False, error
+
+    log_data = prepare_log_data
+    params = {
+        "iter": 100,
+    }
+    logger.set_params(params)
+    logger.hold(est, eval, manager_attr, verbose=True, save=True)
+    assert log_data == logger.get_params()["log_data"]
+    ext = ".pkl"
+    os.remove(os.path.join(save_dir, f_name + ext))
+
+    # check for validation
+    policy = "COD"
+    params = {
+        "policy": policy,
+        "bench_mark": {"metrics": bench_mark}
+    }
+    logger.set_params(params)
+    logger.validate(eval)
+    assert logger.validate(eval), f"{policy} logic is wrong"
+
+    policy = "PINBALL"
+    bench_mark = {k: (v + 1) for k, v in log_data["metrics"].items()}
+    params = {
+        "policy": policy,
+        "bench_mark": {"metrics": bench_mark}
+    }
+    logger.set_params(params)
+    logger.validate(eval)
+    assert logger.validate(eval), f"{policy} logic is wrong"
+
+    # check for 2 steps round training
+    # first round
+    param = {
+        "iter": 100,
+        "first_round": f_round,
+        "bench_mark": {"metrics": bench_mark},
+        }
+    logger.set_params(param)
+    try:
+        logger.log(est, eval, manager_attr)
+    except Exception as error:
+        assert False, error
+
+    # second round
+    param = {
+        "second_round": s_round,
+        "bench_mark": {"metrics": bench_mark},
+        }
+    logger.set_params(param)
+    param = {
+        "mode": s_round,
+        }
+    manager_attr.update(param)
+    try:
+        logger.log(est, eval, manager_attr)
+    except Exception as error:
+        assert False, error
---
+++ tests/test_tornado_module.py
@@ -0,0 +1,198 @@
+import datetime
+import numpy as np
+import pandas as pd
+from itertools import combinations
+from cyclic_boosting.tornado.core.module import (
+    TornadoModule,
+    ForwardSelectionModule,
+    BFForwardSelectionModule
+)
+from cyclic_boosting.smoothing.onedim import SeasonalSmoother, IsotonicRegressor
+import pytest
+
+
+@pytest.fixture(scope="module")
+def manager_base_params():
+    params = {
+        "mfp": None,
+        "is_ts":  True,
+        "data_interval": None,
+        "task": "regression",
+        "dist": "poisson",
+        "model": "multiplicative",
+        "X": None,
+        "y": None,
+        "target": None,
+        "regressor": None,
+        "features": list(),
+        "interaction_term": list(),
+        "feature_properties": dict(),
+        "smoothers": dict(),
+        "observers": dict(),
+        "report": dict(),
+        "init_model_attr": dict(),
+        "model_params": dict(),
+        "max_interaction": 0,
+        "experiment": 0,
+        "end": 0
+    }
+    return params
+
+
+def test_tornado_module(prepare_data, manager_base_params):
+    X, y = prepare_data
+    X, y = X[:100].copy(), y[:100].copy()
+    X.loc[:, "SALES"] = y.copy()
+    names = {k: k.lower() for k in X.columns}
+    X.rename(names, axis=1, inplace=True)
+
+    length = 10
+    data = np.arange(length).astype(np.int64)
+    start_date = datetime.datetime.today().date()
+    dates = pd.date_range(start=start_date, periods=length, freq='D')
+    X_dummy = pd.DataFrame(
+        {
+            "int": data,
+            "float": data.astype(np.float64),
+            "object": data.astype(object),
+            "date": dates
+        }
+    )
+    manager = TornadoModule(is_time_series=True)
+
+    # get_params
+    params = manager.get_params()
+    for p, desired in zip(params.items(), manager.get_params().items()):
+        assert p[1] == desired[1], p[0]
+
+    # set_params
+    manager_base_params.update({"end": 10000, "X": X_dummy})
+    manager.set_params(manager_base_params)
+    params = manager.get_params()
+    assert params["end"] == manager_base_params["end"]
+
+    # feature_properties
+    attr = "feature_properties"
+    manager_base_params.update({"end": 0, "X": X})
+    manager.set_params(manager_base_params)
+    manager.set_feature_property()
+
+    # set_feature
+    func = "set_feature"
+    attr = "features"
+    desired = ["int", "float", "object"]
+    manager_base_params.update({"interaction_term": [("a", "b")]})
+    manager.set_params(manager_base_params)
+    manager.set_feature(desired)
+    desired.append(("a", "b"))
+    res = manager.get_params()
+    is_same = res[attr] == desired
+    assert is_same, f"{func}'s result is {res[attr]}, {desired}"
+
+    # set_interaction_term
+    func = "set_interaction_term"
+    attr = "interaction_term"
+    desired = [x for x in combinations(X_dummy.columns, 2)]
+    manager_base_params.update({
+        "init_model_attr": {"features": list(X_dummy.columns)},
+        "X": X_dummy,
+        "interaction_term": list()
+        })
+    manager.set_params(manager_base_params)
+    manager.set_interaction_term()
+    res = manager.get_params()
+    is_same = res[attr] == desired
+    assert is_same, f"{func}'s result is {res[attr]}, {desired}"
+
+    # set_smoother
+    func = "set_smoother"
+    attr = "smoothers"
+    report_dummy = {
+        "has_seasonality": ["seasonal"],
+        "has_up_monotonicity": ["up"],
+        "has_down_monotonicity": ["down"]
+    }
+    manager_base_params.update({"report": report_dummy})
+    manager.set_params(manager_base_params)
+    manager.set_smoother()
+    res = list(manager.get_params()[attr].values())
+    assert isinstance(res[0], SeasonalSmoother)
+    assert isinstance(res[1], IsotonicRegressor)
+    assert isinstance(res[2], IsotonicRegressor)
+
+    # set observer
+    manager.set_observer()
+
+    # drop_unused_features
+    func = "drop_unused_features"
+    attr = "X"
+    param = {
+        "init_model_attr": {"X": X_dummy},
+        "feature_properties": {"int": 1},
+    }
+    manager.set_params(param)
+    manager.drop_unused_features()
+    res = manager.get_params()
+    desired = ["int"]
+    is_same = list(res[attr].columns) == desired
+    assert is_same, f"{func}'s result is {res[attr].columns}, {desired}"
+
+    # clear
+    func = "clear"
+    manager.clear()
+    assert isinstance(manager.features, list)
+    assert isinstance(manager.feature_properties, dict)
+    assert isinstance(manager.smoothers, dict)
+    assert isinstance(manager.observers, dict)
+
+    # basic steps
+    manager.init(X.copy(), "SALES")
+    manager.update()
+    manager.manage()
+
+
+def test_forward_selection_module(prepare_data):
+    X, y = prepare_data
+    X, y = X[:100].copy(), y[:100].copy()
+    X.loc[:, "SALES"] = y.copy()
+    names = {k: k.lower() for k in X.columns}
+    X.rename(names, axis=1, inplace=True)
+    manager = ForwardSelectionModule(is_time_series=True)
+
+    # init
+    manager.init(X.copy(), "SALES")
+
+    # feature
+    desired = ["dummy"]
+    manager.set_feature(desired)
+    assert manager.features == desired
+
+    # manage
+    manager.manage()
+
+    # update
+    manager.update()
+
+"""
+def test_bfforward_selection_module(prepare_data):
+    X, y = prepare_data
+    X, y = X[:100].copy(), y[:100].copy()
+    X.loc[:, "SALES"] = y
+    names = {k: k.lower() for k in X.columns}
+    X.rename(names, axis=1, inplace=True)
+    manager = BFForwardSelectionModule(is_time_series=True)
+
+    # init
+    manager.init(X.copy(), "SALES")
+
+    # feature
+    desired = ["dummy"]
+    manager.set_feature(desired)
+    assert manager.features == desired
+
+    # manage
+    manager.manage()
+
+    # update
+    manager.update()
+"""
