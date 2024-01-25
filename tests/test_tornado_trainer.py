dfcb71852a7d2d0cfaeefbe84b3fcfdfb0108372
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2024-01-25 19:46:53 +0900

    [new] simple integration test for tornado

---
+++ tests/test_tornado_trainer.py
@@ -0,0 +1,38 @@
+from cyclic_boosting.tornado.core import datamodule, module
+from cyclic_boosting.tornado.trainer import trainer
+
+
+def test_tornado(prepare_data):
+    X, y = prepare_data
+    df, target = X.iloc[:200].copy(), y[:200].copy()
+    df.loc[:, "sales"] = target
+    df.to_csv("temp.csv", index=False)
+
+    data_deliverer = datamodule.TornadoDataModule("temp.csv")
+    manager = module.TornadoModule(max_iter=5)
+    predictor = trainer.Tornado(data_deliverer, manager)
+    predictor.fit(target="sales", log_policy="COD", verbose=False)
+
+
+def test_forward_trainer(prepare_data):
+    X, y = prepare_data
+    df, target = X.iloc[:200].copy(), y[:200].copy()
+    df.loc[:, "sales"] = target
+    df.to_csv("temp.csv", index=False)
+
+    data_deliverer = datamodule.TornadoDataModule("temp.csv")
+    manager = module.ForwardSelectionModule(max_iter=5)
+    predictor = trainer.ForwardTrainer(data_deliverer, manager)
+    predictor.fit(target="sales", log_policy="COD", verbose=False)
+
+
+def test_qpd_forward_trainer(prepare_data):
+    X, y = prepare_data
+    df, target = X.iloc[:200].copy(), y[:200].copy()
+    df.loc[:, "sales"] = target
+    df.to_csv("temp.csv", index=False)
+
+    data_deliverer = datamodule.TornadoDataModule("temp.csv")
+    manager = module.BFForwardSelectionModule(max_iter=10, model="additive")
+    predictor = trainer.QPDForwardTrainer(data_deliverer, manager)
+    predictor.fit(target="sales", log_policy="PINBALL", verbose=False)
