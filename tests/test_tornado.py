5d620adaece020b584379ef940002ec876ede419
setoguchi-naoki <setoguchi.naoki@jp.panasonic.com>
2024-03-25 20:23:06 +0900

    take DataModule out

--- tests/test_tornado.py
+++ tests/test_tornado.py
@@ -1,5 +1,6 @@
 from cyclic_boosting.tornado.core import datamodule, manager
 from cyclic_boosting.tornado.trainer import tornado
+from sklearn.model_selection import train_test_split
 import shutil
 
 
@@ -7,13 +8,16 @@ def test_interaction_search_model(prepare_data):
     X, y = prepare_data
     df, target = X.iloc[:200].copy(), y[:200].copy()
     df.loc[:, "sales"] = target
+    df_train, df_test = train_test_split(df, test_size=0.1, shuffle=False)
 
-    data_deliverer = datamodule.TornadoDataModule(df)
+    data_deliverer = datamodule.TornadoDataModule(df_train)
+    train, validation = data_deliverer.generate_trainset(test_size=0.2, seed=0, target="sales", is_time_series=True)
+    test = data_deliverer.generate_testset(df_test)
     manager_ = manager.TornadoManager(max_iter=5)
-    predictor = tornado.InteractionSearchModel(data_deliverer, manager_)
-    predictor.fit(target="sales", criterion="COD", verbose=False)
-    _ = predictor.predict(df)
-    _ = predictor.predict_proba(df, output="proba")
+    predictor = tornado.InteractionSearchModel(manager_)
+    predictor.fit("sales", train, validation, criterion="COD", verbose=False)
+    _ = predictor.predict(test)
+    _ = predictor.predict_proba(test, output="proba")
     shutil.rmtree("./models")
 
 
@@ -21,13 +25,17 @@ def test_forward_selection_model(prepare_data):
     X, y = prepare_data
     df, target = X.iloc[:200].copy(), y[:200].copy()
     df.loc[:, "sales"] = target
+    df_train, df_test = train_test_split(df, test_size=0.1, shuffle=False)
 
-    data_deliverer = datamodule.TornadoDataModule(df)
+    data_deliverer = datamodule.TornadoDataModule(df_train)
+    train, validation = data_deliverer.generate_trainset(test_size=0.2, seed=0, target="sales", is_time_series=True)
+    test = data_deliverer.generate_testset(df_test)
+
     manager_ = manager.ForwardSelectionManager(max_iter=5)
-    predictor = tornado.ForwardSelectionModel(data_deliverer, manager_)
-    predictor.fit(target="sales", criterion="COD", verbose=False)
-    _ = predictor.predict(df)
-    _ = predictor.predict_proba(df, output="proba")
+    predictor = tornado.ForwardSelectionModel(manager_)
+    predictor.fit("sales", train, validation, criterion="COD", verbose=False)
+    _ = predictor.predict(test)
+    _ = predictor.predict_proba(test, output="proba")
     shutil.rmtree("./models")
 
 
@@ -35,28 +43,36 @@ def test_prior_pred_interaction_search_model(prepare_data):
     X, y = prepare_data
     df, target = X.iloc[:200].copy(), y[:200].copy()
     df.loc[:, "sales"] = target
+    df_train, df_test = train_test_split(df, test_size=0.1, shuffle=False)
 
-    data_deliverer = datamodule.TornadoDataModule(df)
+    data_deliverer = datamodule.TornadoDataModule(df_train)
+    train, validation = data_deliverer.generate_trainset(test_size=0.2, seed=0, target="sales", is_time_series=True)
+    test = data_deliverer.generate_testset(df_test)
+
     manager_ = manager.PriorPredForwardSelectionManager(max_iter=5)
-    predictor = tornado.PriorPredInteractionSearchModel(data_deliverer, manager_)
-    predictor.fit(target="sales", criterion="COD", verbose=False)
-    _ = predictor.predict(df)
-    _ = predictor.predict_proba(df, output="proba")
+    predictor = tornado.PriorPredInteractionSearchModel(manager_)
+    predictor.fit("sales", train, validation, criterion="COD", verbose=False)
+    _ = predictor.predict(test)
+    _ = predictor.predict_proba(test, output="proba")
     shutil.rmtree("./models")
 
 
 def test_qpd_interaction_search_model(prepare_data):
     X, y = prepare_data
-    df, target = X.iloc[:2000].copy(), y[:2000].copy()  # need a dataset included enough data size to train
+    df, target = X.iloc[:3000].copy(), y[:3000].copy()  # need a dataset included enough data size to train
     df.loc[:, "sales"] = target
+    df_train, df_test = train_test_split(df, test_size=0.1, shuffle=False)
 
-    data_deliverer = datamodule.TornadoDataModule(df)
+    data_deliverer = datamodule.TornadoDataModule(df_train)
+    train, validation = data_deliverer.generate_trainset(test_size=0.2, seed=0, target="sales", is_time_series=True)
+    test = data_deliverer.generate_testset(df_test)
+
     manager_ = manager.PriorPredForwardSelectionManager(
         max_iter=10,
         dist="qpd",
     )
-    predictor = tornado.QPDInteractionSearchModel(data_deliverer, manager_, bound="S", lower=0)
-    predictor.fit(target="sales", criterion="PINBALL", verbose=False)
-    _ = predictor.predict(df)
-    _ = predictor.predict_proba(df, output="proba")
+    predictor = tornado.QPDInteractionSearchModel(manager_, bound="S", lower=0)
+    predictor.fit("sales", train, validation, criterion="PINBALL", verbose=False)
+    _ = predictor.predict(test)
+    _ = predictor.predict_proba(test, output="proba")
     shutil.rmtree("./models")
